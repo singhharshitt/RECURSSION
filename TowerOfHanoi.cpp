15732ca4c386a0f11960b12091039ab223849fa6
Harshit singh <149008610+singhharshitt@users.noreply.github.com>
2024-10-23 22:28:49 +0530

    Update TowerOfHanoi.cpp

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -18,3 +18,25 @@ int main(){
     toh(n,'A', 'C', 'B');
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// void towerofHanoi(int n, char source, char destination, char aux){
+//     //if my sourrce have only one element then it will directly move to destnation
+//     if(n==1){
+//         cout<<"Move disk 1 from "<<source<<" to "<<destination<<endl;
+//         return;
+//     }
+//     //Now if i have more than one element the i have to shift from source to aux then then to destination
+    
+//     towerofHanoi(n-1, source, aux, destination);
+//     cout<<"Move disk "<<n<<" from "<<source<<" to "<<destination<<endl;
+//     towerofHanoi(n-1, aux, destination, source);
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter number of elements source has: ";
+//     cin>>n;
+//     towerofHanoi(n, 'A', 'B', 'C');
+// }
